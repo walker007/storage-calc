9349c94df3bb8a2df7a5c3bec289d088ee11000c
devmath <midia.matheus@gmail.com>
2016-06-04 09:34:35 -0300

    updates

--- js/calc.js
+++ js/calc.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
-	$("input[type='submit']").click(function() {
+	$("button").click(function() {
 		var number = $("input[type='number']").val();
-		var number = number * 1024;
 
 		var song = 3;
 		var picture = 2;
 		var video = 60;
 
-		for (var i = song; i = number; i++) {
-			if (i == number) {
+
+		for (var i = 1; i == number; i++) {
+			res = i * song;
+			if (res >= number) {
 				$("#files ul li:first-child span").prepend("Foi!");
-			} else {
-				i++;
 			}
 		}
 	});
